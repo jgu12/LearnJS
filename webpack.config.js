e7e70d84c99c283be8a7510caf2765dfbd75e525
yifan <jack_gu@live.com>
2020-09-19 16:12:08 -0700

    configs as in tutorial

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve('public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  }
+};
+
+/*
+bundle all modules we denpend on inisde the entry index.js
+into a bundle.ks file under public directory
+Then for every js file we run bable loader on them, which
+transfroms the non-standard JS
+*/
