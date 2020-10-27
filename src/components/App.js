c9c534cb04811b6868bf351b472191f48f460c60
Jack Gu <jack_gu@live.com>
2020-10-26 21:01:45 -0700

    save

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,40 @@ import React from 'react';
 // import PropTypes from 'prop-types';
 import Header from './Header';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header message = 'Header message!'/>
-      <div>
-        .....
+//if need state & lifecyle
+class App extends React.Component {
+  state = {
+    pageHeader: 'Naming Contests'
+  };
+
+  //componet lifecyle hooks:
+  // componentDidMount(){
+  //   //usually do ajax fetching, when got something back from the server, 
+  //   //we are sure we have the target in dom to modify
+  //   //also timers, listeners.. and clean up in componentWillUnmount
+  // }
+  render() {
+    return (
+      <div className="App">
+        <Header message={this.state.pageHeader} />
+        <div>
+          .....
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+// if just need stateless, use this sytanx is enough
+// const App = () => {
+//   return (
+//     <div className="App">
+//       <Header message='Header message!' />
+//       <div>
+//         .....
+//       </div>
+//     </div>
+//   );
+// };
 
 export default App;
